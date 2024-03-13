7d7b5e4291909108f46c9b2a61f27f69e18e509e
Matt Wyman <matt@okareo.com>
2024-03-12 22:08:04 -0700

    Update to the entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,3 +4,8 @@ echo "Hello $1"
 time=$(date)
 echo "time=$time" >> $GITHUB_OUTPUT
 
+curl -L https://github.com/okareo-ai/okareo-cli/releases/download/v0.0.2/okareo-cli_0.0.2_linux_amd64.tar.gz > cli.tar.gz
+tar -xvf cli.tar.gz
+chmod 755 okareo-cli
+./okareo-cli --help
+
