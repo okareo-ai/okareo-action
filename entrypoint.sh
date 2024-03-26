07d951a778944eb1232cdd0eee0123cd961dcadf
Matt Wyman <matt@okareo.com>
2024-03-26 12:12:40 -0700

    Update to action

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,5 +6,5 @@ echo "time=$time" >> $GITHUB_OUTPUT
 
 curl -L https://github.com/okareo-ai/okareo-cli/releases/download/v0.0.8/okareo_0.0.8_linux_amd64.tar.gz > okareo.tar.gz
 tar -xvf okareo.tar.gz
-chmod 755 bin/okareo
+chmod 777 bin/okareo
 ./bin/okareo --help
