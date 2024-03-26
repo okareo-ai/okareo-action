3c99f6241088523d92afec773eeaa8faa942a8c3
Matt Wyman <matt@okareo.com>
2024-03-26 11:57:40 -0700

    Updates to Okareo Action based on CLI refactor.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,10 @@
 #!/bin/sh -l
 
-echo "Hello $1"
+echo "Okareo: $1"
 time=$(date)
 echo "time=$time" >> $GITHUB_OUTPUT
 
-curl -L https://github.com/okareo-ai/okareo-cli/releases/download/v0.0.7/okareo-cli_0.0.7_linux_amd64.tar.gz > cli.tar.gz
-tar -xvf cli.tar.gz
-chmod 755 okareo-cli
-./okareo-cli --help
-
+curl -L https://github.com/okareo-ai/okareo-cli/releases/download/v0.0.8/okareo_0.0.8_linux_amd64.tar.gz > okareo.tar.gz
+tar -xvf okareo.tar.gz
+chmod 755 bin/okareo
+./bin/okareo --help
